400f1d02eab843177849abc174aeffac123df2cd
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-10-26 00:50:53 -0500

    Set general postgres database details

--- parkingapp/parkingapp/settings.py
+++ parkingapp/parkingapp/settings.py
@@ -93,11 +93,10 @@ DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
           # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'parkingdb',                      # Or path to database file if using sqlite3.
+        'NAME': 'testdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'mdallman2570',
+        'USER': 'test',
         'PASSWORD': 'test',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '', 
