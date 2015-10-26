2a20f1904538a6b1ea82d279cad1f86bfef6ed89
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-10-26 13:12:02 -0500

    Added fields to review model. Reversed Review and ParkingSpot relationship.

--- parkingapp/parkingspot/models.py
+++ parkingapp/parkingspot/models.py
@@ -2,22 +2,23 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.gis.db import models
 from userprof.models import AdminUser
-from review.models import Review
+#from review.models import Review
 from django.conf import settings
 # Create your models here.
 
 class ParkingSpot(models.Model):
-  street_address = models.CharField(max_length=70, blank=True)
-  city = models.CharField(max_length = 30, blank = True)
-  state = models.CharField(max_length = 2, blank = True)
-  zipcode = models.IntegerField(blank=True)
-  location = models.PointField(null=True,blank=True)
-  owner = models.ForeignKey(AdminUser)
-  description = models.CharField(max_length=256)
-  amenities = ArrayField(models.CharField(max_length=80, blank=True), blank=True, null=True, size=7)
-  photos = models.ImageField(default='%s/default.png' % settings.MEDIA_URL)
-  reviews = ArrayField(models.CharField(max_length=80,blank=True), blank=True, null=True)
-  objects = models.GeoManager()
+    street_address = models.CharField(max_length=70, blank=True)
+    city = models.CharField(max_length = 30, blank = True)
+    state = models.CharField(max_length = 2, blank = True)
+    zipcode = models.IntegerField(blank=True)
+    location = models.PointField(null=True,blank=True)
+    owner = models.ForeignKey(AdminUser)
+    description = models.CharField(max_length=256)
+    amenities = ArrayField(models.CharField(max_length=80, blank=True), blank=True, null=True, size=7)
+    photos = models.ImageField(default='%s/default.png' % settings.MEDIA_URL)
+    # reviews are a one-to-many relationship
+    #reviews = ArrayField(models.CharField(max_length=80,blank=True), blank=True, null=True)
+    objects = models.GeoManager()
 
 
 
--- parkingapp/review/models.py
+++ parkingapp/review/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from parkingspot.models import ParkingSpot
+from userprof.models import AdminUser
 
 # Create your models here.
 class Review(models.Model):
-  name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80)
+    parkingspot = models.ForeignKey(ParkingSpot, default=None)
+    reviewer = models.ForeignKey(AdminUser, default=None)
+    headline = models.CharField(max_length=40, blank=True)
+    review_text = models.CharField(max_length=400, blank=True)
+    rating = models.IntegerField(default=5)
