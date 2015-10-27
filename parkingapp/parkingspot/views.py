09b8936cf49675955c83b8ab6f4f8952bb3ff341
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-10-26 22:53:30 -0500

    Added parkingspot serialization and usage in template example

--- parkingapp/parkingspot/views.py
+++ parkingapp/parkingspot/views.py
@@ -1,8 +1,14 @@
+from django.core import serializers
 from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
 from parkingspot.models import ParkingSpot
 
 # Create your views here.
 def home(request):
-    context = { 'parkingspots' : ParkingSpot.objects.all() }
+    parkingspots = ParkingSpot.objects.all()
+    json_parkingspots = serializers.serialize("json", parkingspots)
+    context = {
+        'parkingspots' : parkingspots,
+        'json_parkingspots' : json_parkingspots
+    }
     return render(request, 'base.html', context)
