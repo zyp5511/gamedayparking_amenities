2b7ddf98fccff423787c6a57e185aa61c4fc11be
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-10-26 21:48:46 -0500

    Added simple example of how to access and use parkingspots passed from the views

--- parkingapp/parkingspot/views.py
+++ parkingapp/parkingspot/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
+from parkingspot.models import ParkingSpot
 
 # Create your views here.
-
 def home(request):
-  array = ['My', 'name', 'is', 'matt']
-  return redirect('accounts/login/')
+    context = { 'parkingspots' : ParkingSpot.objects.all() }
+    return render(request, 'base.html', context)
--- parkingapp/userprof/admin.py
+++ parkingapp/userprof/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from userprof.models import AdminUser
 
 # Register your models here.
+admin.site.register(AdminUser)
