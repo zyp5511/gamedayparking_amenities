d75cd72491bcd28c9addf373ffc08f14a5b13a64
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-10-30 01:09:57 -0500

    Edited ParkingSpot Model Fields

--- parkingapp/parkingspot/models.py
+++ parkingapp/parkingspot/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
+from localflavor.us.us_states import STATE_CHOICES
 from userprof.models import AdminUser
 from geopy.geocoders import GoogleV3
 from django.conf import settings
@@ -10,12 +11,13 @@ from django.conf import settings
 class ParkingSpot(models.Model):
     street_address = models.CharField(max_length=70, blank=True)
     city = models.CharField(max_length = 30, blank = True)
-    state = models.CharField(max_length = 2, blank = True)
-    zipcode = models.IntegerField(blank=True)
+    state = models.CharField(max_length = 2, choices=STATE_CHOICES, default="WI")
+    zipcode = models.IntegerField(blank=True, null=True)
     location = models.PointField(null=True,blank=True)
     owner = models.ForeignKey(AdminUser)
     description = models.CharField(max_length=256)
     amenities = ArrayField(models.CharField(max_length=80, blank=True), blank=True, null=True, size=7)
+    cost = models.IntegerField(blank=True, null=True, default=5)
     photos = models.ImageField(default='%s/default.png' % settings.MEDIA_URL)
     objects = models.GeoManager()
 
