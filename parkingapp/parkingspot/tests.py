1b3cca5b9fe502432de4fbfffe0589278daa9d58
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-11-01 22:17:36 -0600

    added selenium testing

--- parkingapp/parkingspot/tests.py
+++ parkingapp/parkingspot/tests.py
@@ -1,3 +1,7 @@
+import time
+
+from selenium.webdriver.firefox.webdriver import WebDriver
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 from django.core.files import File
 from django.test import TestCase
@@ -5,23 +9,38 @@ from parkingspot.models import ParkingSpot
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from geopy.geocoders import GoogleV3
-from userprof.models import AdminUser
+
+from django.contrib.auth.models import User
+from userprof.models import AdminUser, ExtendedUser
+from userprof.tests import create_allauth_user
 
 class ParkingSpotTests(TestCase):
 
     def setUp(self):
-        self.user = AdminUser.objects.create(name="Ryan")
+        user = User.objects.create()
+        user.save()
+        #allauth_user = create_allauth_user("rschaefer@wisc.edu")
+        extended_user = ExtendedUser.objects.create(
+            main_user = user,
+        )
+        extended_user.save()
+        admin_user = AdminUser.objects.create(
+            extended_user = extended_user
+        )
+        admin_user.save()
 
+        #should not be returned when searching around madison
         self.test_spot1 = ParkingSpot.objects.create(
             street_address = "400 Morning Oaks Ct",
             city = "Ellisville",
             state = "MO",
             zipcode = 63021,
             location = Point(-90.586876, 38.592703),
-            owner = self.user,
+            owner = admin_user,
             description = "Missouri Parking Spot",
             amenities =(["test1", "test2", "test3"]),
         )
+        self.test_spot1.save()
 
 
         self.test_spot2 = ParkingSpot.objects.create(
@@ -29,10 +48,11 @@ class ParkingSpotTests(TestCase):
             city = "Madison",
             state = "WI",
             zipcode = 53703,
-            owner = self.user,
+            owner = admin_user,
             description = "Madison Parking Spot. Location Field should be automatically populated",
             amenities =(["test1", "test2", "test3"]),
         )
+        self.test_spot2.save()
 
     def test_city_filter(self):
         """Filters Based On City"""
@@ -49,7 +69,143 @@ class ParkingSpotTests(TestCase):
         self.assertEqual(spots[0].pk, self.test_spot2.pk)
 
 
-
-
     def test_review(self):
+        time.sleep(2)
+
+class SeleniumTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(SeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+
+    def setUp(self):
+        user = User.objects.create()
+        user.save()
+        #allauth_user = create_allauth_user("rschaefer@wisc.edu")
+        extended_user = ExtendedUser.objects.create(
+            main_user = user,
+        )
+        extended_user.save()
+        admin_user = AdminUser.objects.create(
+            extended_user = extended_user
+        )
+        admin_user.save()
+
+        #should not be returned when searching around madison
+        self.test_spot1 = ParkingSpot.objects.create(
+            street_address = "400 Morning Oaks Ct",
+            city = "Ellisville",
+            state = "MO",
+            zipcode = 63021,
+            owner = admin_user,
+            description = "Missouri Parking Spot",
+            amenities =(["test1", "test2", "test3"]),
+        )
+        self.test_spot1.save()
+
+        self.test_spot2 = ParkingSpot.objects.create(
+            street_address = "1440 Monroe Street",
+            city = "Madison",
+            state = "WI",
+            zipcode = 53703,
+            cost = 5,
+            owner = admin_user,
+            description = "has grill, middle cost",
+            amenities = '{"bathroom":false,"yard":false,"grill":true,"table":false,"electricity":false}'
+        )
+        self.test_spot2.save()
+
+
+        self.test_spot3 = ParkingSpot.objects.create(
+            street_address = "Engineering Hall",
+            city = "Madison",
+            state = "WI",
+            zipcode = 53703,
+            cost = 10,
+            owner = admin_user,
+            description = "has bathroom, highest cost",
+            amenities = '{"bathroom":bathroom,"yard":false,"grill":true,"table":false,"electricity":false}'
+        )
+        self.test_spot3.save()
+
+
+        self.test_spot4 = ParkingSpot.objects.create(
+            street_address = "633 N Henry Street",
+            city = "Madison",
+            state = "WI",
+            zipcode = 53703,
+            cost = 0,
+            owner = admin_user,
+            description = "has yard, lowest cost",
+            amenities = '{"bathroom":bathroom,"yard":true,"grill":true,"table":false,"electricity":false}'
+        )
+        self.test_spot4.save()
+
+        self.madison_spots = [self.test_spot2, self.test_spot3, self.test_spot4]
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(SeleniumTests, cls).tearDownClass()
+
+    def test_home_nav_bar(self):
+        # test brand link
+        self.selenium.get("{}{}".format(self.live_server_url, '/home/'))
+        self.selenium.find_element_by_link_text("Game Day Parking").click()
+        title = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(title.text, u'Gameday Amenities Finder')
+        # test home link
+        self.selenium.get("{}{}".format(self.live_server_url, '/home/'))
+        self.selenium.find_element_by_link_text("Home").click()
+        title = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(title.text, u'Gameday Amenities Finder')
+        # test test about us link
+        self.selenium.get("{}{}".format(self.live_server_url, '/home/'))
+        self.selenium.find_element_by_link_text("About Us").click()
+        title = self.selenium.find_element_by_tag_name("h2")
+        self.assertEqual(title.text, u'What does Gameday Parking do?')
+        # test contact us link
+        # link does nothing yet
+
+    def test_home_search(self):
+        self.selenium.get("{}{}".format(self.live_server_url, '/home/'))
+        search_input =  self.selenium.find_element_by_name("location")
+        search_input.clear()
+        search_input.send_keys("Camp Randall")
+        search_input.submit()
+        # verify three of four parkingspots appear
+        spots = [x.text for x in self.selenium.find_elements_by_tag_name("h3")]
+        test_spots = [x.street_address for x in self.madison_spots]
+        self.assertTrue(all(x in spots for x in test_spots))
+
+    def test_search_nav_bar(self):
+        # test brand link
+        self.selenium.get("{}{}".format(self.live_server_url, '/search/?location=Madison%2C+WI'))
+        self.selenium.find_element_by_link_text("Game Day Parking").click()
+        title = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(title.text, u'Gameday Amenities Finder')
+        # test home link
+        self.selenium.get("{}{}".format(self.live_server_url, '/search/?location=Madison%2C+WI'))
+        self.selenium.find_element_by_link_text("Home").click()
+        title = self.selenium.find_element_by_tag_name("h1")
+        self.assertEqual(title.text, u'Gameday Amenities Finder')
+        # test test about us link
+        self.selenium.get("{}{}".format(self.live_server_url, '/search/?location=Madison%2C+WI'))
+        self.selenium.find_element_by_link_text("About Us").click()
+        title = self.selenium.find_element_by_tag_name("h2")
+        self.assertEqual(title.text, u'What does Gameday Parking do?')
+        # test contact us link
+        # link does nothing yet
+
+    def test_search_filter(self):
+        # test filtering the spots
+        self.selenium.get("{}{}".format(self.live_server_url, '/search/?location=Madison%2C+WI'))
+
+    def test_search_sort(self):
         pass
+
+
+    def test_search_filter_and_sort(self):
+        pass
+
+
--- parkingapp/userprof/tests.py
+++ parkingapp/userprof/tests.py
@@ -1,3 +1,73 @@
 from django.test import TestCase
 
+import random
+import string
+
+from django.conf import settings
+from django.http import HttpRequest, QueryDict
+from django.utils.importlib import import_module
+
+from allauth.account import app_settings, signals
+from allauth.account.forms import SignupForm
+from allauth.account.views import SignupView
+from allauth.account.utils import setup_user_email, complete_signup
+
+
+def generate_password(chars=8):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(chars))
+
+
+def get_post_dic(email):
+    password = generate_password()
+    #csrfmiddlewaretoken = #not necessary
+    return {
+            u'password1': password,
+            #u'csrfmiddlewaretoken': #not necessary
+            u'email': email,
+            u'password2': password,
+            u'confirmation_key': ""
+    }
+
+
+def get_signin_request(post_dic):
+    signin_request = HttpRequest()
+
+    query_dict = QueryDict("")
+    query_dict_post = query_dict.copy() # Make it mutable
+    query_dict_get = query_dict.copy() # Make it mutable
+
+    query_dict_post.update(post_dic)
+
+    signin_request.POST = query_dict_post
+    signin_request.GET = query_dict_get
+
+    # Set session (fails without)
+    # http://stackoverflow.com/questions/16865947/django-httprequest-object-has-no-attribute-session
+    engine = import_module(settings.SESSION_ENGINE)
+    session_key = None
+    signin_request.session = engine.SessionStore(session_key)
+
+    signin_request.SERVER_NAME = ""
+
+    return signin_request
+
+
+def create_allauth_user(email):
+    post_dic = get_post_dic(email)
+    signin_request = get_signin_request(post_dic)
+
+    signup_form_kwargs = {"files": {}, "initial": {},
+                          "data": signin_request.POST}
+    # From allauth.account.views.SignupView.form_valid
+    signup_form = SignupForm(**signup_form_kwargs)
+    signup_form.cleaned_data = post_dic # Csrf token not neccesary either
+    user = signup_form.save(signin_request)
+
+    # Sends sign up mail
+    signals.user_signed_up.send(sender=user.__class__,
+                                request=signin_request,
+                                user=user,
+                                signal_kwargs={})
+    return user
+
 # Create your tests here.
