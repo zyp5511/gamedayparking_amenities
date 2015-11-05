368b8f002a94bfaca21bf1717e6c15ae0d5cee1d
Ryan Schaefer <ryanschaefer23@gmail.com>
2015-11-04 20:49:53 -0600

    Add reservation system testing

--- parkingapp/parkingspot/tests.py
+++ parkingapp/parkingspot/tests.py
@@ -15,11 +15,11 @@ from jsonfield import JSONField
 class ParkingSpotTests(TestCase):
 
     def setUp(self):
-        user = User.objects.create()
-        user.save()
+        self.user = User.objects.create()
+        self.user.save()
         #allauth_user = create_allauth_user("rschaefer@wisc.edu")
         self.extended_user = ExtendedUser.objects.create(
-            main_user = user,
+            main_user = self.user,
         )
         self.extended_user.save()
         admin_user = AdminUser.objects.create(
@@ -36,7 +36,7 @@ class ParkingSpotTests(TestCase):
             location = Point(-90.586876, 38.592703),
             owner = admin_user,
             description = "Missouri Parking Spot",
-            amenities =(["test1", "test2", "test3"]),
+            default_num_spots = 5
         )
         self.test_spot1.save()
 
@@ -48,7 +48,6 @@ class ParkingSpotTests(TestCase):
             zipcode = 53703,
             owner = admin_user,
             description = "Madison Parking Spot. Location Field should be automatically populated",
-            amenities =(["test1", "test2", "test3"]),
         )
         self.test_spot2.save()
 
@@ -65,7 +64,6 @@ class ParkingSpotTests(TestCase):
         spots = ParkingSpot.objects.filter(location__distance_lte=(point, D(mi=10)))
         self.assertEqual(len(spots), 1)
         self.assertEqual(spots[0].pk, self.test_spot2.pk)
-
 
 
     def test_zero_ratings(self):
@@ -109,3 +107,36 @@ class ParkingSpotTests(TestCase):
         review1.delete()
         review2.delete()
         review3.delete()
+
+
+    def test_make_reservation_closed_date(self):
+        ret = self.test_spot1.reserve_spot(self.user, '12/31/2016')
+        self.assertEqual(ret, -1)
+
+    def test_make_reservation_open_date(self):
+        self.test_spot1.default_num_spots = 1
+        self.test_spot1.open_date('12/31/2016')
+        ret = self.test_spot1.reserve_spot(self.user.id, '12/31/2016')
+        self.assertEqual(ret, 0)
+        self.clear_dates(self.test_spot1)
+
+    def test_fill_reservation(self):
+        test_date = '12/31/2016'
+        self.test_spot1.default_num_spots = 2
+        self.test_spot1.open_date(test_date)
+        ret = self.test_spot1.reserve_spot(self.user.id, test_date)
+        self.assertEqual(ret, 0)
+        self.assertEqual(self.test_spot1.get_num_spots(test_date), 1)
+        ret = self.test_spot1.reserve_spot(self.user.id, test_date)
+        self.assertEqual(self.test_spot1.get_num_spots(test_date), 0)
+        self.clear_dates(self.test_spot1)
+
+    def test_cancel_empty_reservation(self):
+        pass
+
+    def test_cancel_existing_reservation(self):
+        pass
+
+    def clear_dates(self, spot):
+        spot.dates = '{"dates":{}}'
+        spot.save()
