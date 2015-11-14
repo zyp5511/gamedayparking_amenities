3ded25c5d25be842a73f4b162a8a6eec622a8ed8
Matt Dallman <mdallman2570@aol.com>
2015-11-14 13:13:42 -0600

    test

---
+++ parkingapp/parkingspot/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from parkingspot.models import ParkingSpot
+
+
+class ParkingSpotEdit(forms.ModelForm):
+
+  class Meta:
+    model = ParkingSpot
+    exclude = ['location', 'amenities', 'parking_spot_avail']
+class ImageUploadForm(forms.Form):
+    """Image upload form."""
+    image = forms.ImageField()
