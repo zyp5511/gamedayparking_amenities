e48345b5af6274bf6ecb9b1a3ac1ac1d60390bf2
Matt Dallman <mdallman2570@aol.com>
2015-12-06 21:15:58 -0600

    Multi form issue

--- parkingapp/review/views.py
+++ parkingapp/review/views.py
@@ -7,17 +7,17 @@ from message.models import ResMessage
 @login_required
 def submit_review(request):
   if request.method == 'POST':
-    value = request.POST.get("review")
+    value = request.POST['reviewButton']
     res_msg = ResMessage.objects.get(id=value)
     reviewer = request.user
-    headline = request.POST['headline']
-    review_text = request.POST['review_text']
-    rating = request.POST['rating']
+    headline = request.POST['reviewTitle']
+    review_text = request.POST['reviewmessage']
+    rating = request.POST['ratingSelector']
     rev = Review.objects.create(reviewer=reviewer, headline=headline, review_text=review_text, rating=rating, parkingspot=res_msg.parkingspot)
     res_msg.reviewed = rev
     res_msg.save()
     request.session['message'] = 'Review Added Successfully'
     request.session['message_type'] = True
-    return redirect('/reservations')
+    return redirect('/profile')
 
 
